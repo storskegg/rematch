794f06b2b5c33923f134b3f08009f0e2871e4b77
Liam Conrad <liam@storskegg.org>
2022-01-31 18:39:42 -0600

    add options for posix regex and print all matches

--- main.go
+++ main.go
@@ -2,23 +2,38 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func printUsage() {
 	fmt.Println("rematch is intended to be used with piped input, and a valid regex must be provided as its only argument.")
-	fmt.Println("Example Usage: fortune | rematch '^\\d+'")
+	fmt.Println("Example Usage: fortune | rematch [--option] '^\\d+'")
+
+	flag.VisitAll(func(f *flag.Flag) {
+		fmt.Printf("  %s\t%s\n", f.Name, f.Usage)
+	})
 }
 
 func main() {
-	if len(os.Args) != 2 {
+	// Our Flags
+	var posix, all bool
+
+	flag.BoolVar(&posix, "posix", false, "Use POSIX regex.  Defaults to PCRE")
+	flag.BoolVar(&all, "all", false, "Return all matches. Defaults to the first match")
+
+	flag.Parse()
+
+	if flag.NArg() < 1 {
 		printUsage()
 		return
 	}
 
+	// Check stdin for piped input
 	info, err := os.Stdin.Stat()
 	if err != nil {
 		panic(err)
@@ -29,26 +44,43 @@ func main() {
 		return
 	}
 
+	// Capture piped input, capturing runes
 	reader := bufio.NewReader(os.Stdin)
-	var output []rune
+	captured := &strings.Builder{}
 
 	for {
 		input, _, err := reader.ReadRune()
 		if err != nil && err == io.EOF {
 			break
 		}
-		output = append(output, input)
+		captured.WriteRune(input)
 	}
 
-	haystack := string(output)
+	// Compile our regex argument
+	reString := flag.Args()[flag.NArg()-1]
 
-	first := os.Args[1]
+	var re *regexp.Regexp
 
-	re, err := regexp.Compile(first)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+	if posix {
+		re, err = regexp.CompilePOSIX(reString)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	} else {
+		re, err = regexp.Compile(reString)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
 	}
 
-	fmt.Println(re.FindString(haystack))
+	// Print the first or all matches (as captures)
+	if all {
+		for _, m := range re.FindAllString(captured.String(), -1) {
+			fmt.Println(m)
+		}
+	} else {
+		fmt.Println(re.FindString(captured.String()))
+	}
 }
